21f4c3d79c759a32d557a21d228cbb5eadf0610c
Wilian Zembrani <Wilian3355@gmail.com>
2024-07-01 21:25:42 -0300

    Criando o campo owner usando o model User

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     class Meta:
-        verbose_name = 'Category'
+        verbose_name =  'Category'
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length= 50)
@@ -27,6 +28,11 @@ class Contact(models.Model):
         Category,
         on_delete=models.SET_NULL,
         blank=True, null=True
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
         )
 
     def __str__(self) -> str:
