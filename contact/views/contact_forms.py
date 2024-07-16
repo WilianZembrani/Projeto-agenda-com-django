afc322647fbb57b8e18c5b0f1bc0d154f02eafcb
Wilian Zembrani <Wilian3355@gmail.com>
2024-07-15 21:38:00 -0300

    atualizando

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
-from django.core.paginator import Paginator
 from contact.models import Contact
-from django import forms
-from django.core.exceptions import ValidationError
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-            )
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        
-        return super().clean()
+from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
